7f1d4387ac586ef1d38d811b9e09b98b31d8e11c
Karol Szurkowski <kszurkowski@gmail.com>
2022-09-11 18:20:28 +0200

    solution is bitwise operations.

--- task_4/simple.py
+++ task_4/simple.py
@@ -1,3 +1,4 @@
+import math
 import time
 
 def get_dist_to_bytes(pellets_now):
@@ -15,6 +16,63 @@ def get_dist_to_bytes(pellets_now):
 
 
 def solution(n):
+    """
+    consider bitwise operations and problem of getting the fastest way to a byte
+    """
+    steps = 0
+    n = int(n)
+    while n > 1:
+        steps += 1
+        if n % 2 == 0:
+            n /= 2
+        elif n == 3 or n % 4 == 1:  # preferable minus --> n%4==1 means LSB '001', '101'
+            n -= 1
+        else:  # add only for LSB '111'
+            n += 1
+    return steps
+
+def solution_bitwise1(n):
+    """
+    consider bitwise operations and problem of getting the fastest way to a byte
+    """
+    steps = 0
+    n = int(n)
+    while n > 1:
+        if n % 2 == 0:
+            n = n / 2
+        elif bin(n)[-3:] == '111':  # in this case its better to do + and the consequential divisions
+            n += 1
+        else:
+            n -= 1
+        steps += 1
+    return steps
+
+
+def solution_bitwise2(n):
+
+    # consider the LSBs -- just 3 and always prioritize division
+    n = int(n)
+    steps = 0
+    while n > 1:
+
+        if n % 2 == 0:
+            n /= 2
+
+        else:
+            num_b = bin(n).split('b')[1:][0]
+            lsb_s = num_b[:3]
+
+            if lsb_s == '111':
+                n += 1
+            else:
+                n -= 1
+
+
+        steps += 1
+    return steps
+
+
+def solution_naive(n):
 
     n = int(n)
     step = 0
@@ -47,11 +105,10 @@ def solution(n):
                     n += 1
 
                 # dont select the next value if the next one is ODD
-                elif (n-1)%2 == 0:
-                    n -= 1
-
-                elif (n+1)%2 == 0:
+                elif (n + 1) % 2 == 0:
                     n += 1
+                elif (n - 1) % 2 == 0:
+                    n -= 1
 
                 # follow to the next bytes
                 elif dist_l_l == 1:
@@ -65,19 +122,17 @@ def solution(n):
         # print 'n', n
         step += 1
         is_solved = n==1
+        print 'steps', step , 'n', n
 
     # print n, 'step', step
     return step
 
 
-
-
-
 if __name__ == "__main__":
-    test_inputs = ['4', '15', '77', '135', '217', '314', '2137', '213789']
-    test_outputs = [2, 5, 9, 9, 11, 11, 15, 25]
-    # test_inputs = ['135']
-    # test_outputs = [9]
+    # test_inputs = ['4', '15', '77', '135', '217', '314', '2137', '213789']
+    # test_outputs = [2, 5, 9, 9, 11, 11, 15, 24]
+    test_inputs = ['213789']
+    test_outputs = [24]
     for ipt, opt in zip(test_inputs, test_outputs):
         # print 'ipt', ipt, 'sol', sol, 'opt', opt
         print 'ipt', ipt
@@ -85,3 +140,4 @@ if __name__ == "__main__":
         # sol = None
         print('sol', sol, 'opt', opt)
         print( '\n')
+
